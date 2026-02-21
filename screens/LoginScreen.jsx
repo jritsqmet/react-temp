eb1a0ed81919635bf1d4dc6cc34fe3bb3882a1e5
Hermer2007 <aleherediamm07@gmail.com>
2026-02-21 12:41:21 -0500

    primer comit

--- screens/LoginScreen.jsx
+++ screens/LoginScreen.jsx
@@ -1,10 +1,13 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, TextInput, View } from 'react-native'
 import React from 'react'
 
 export default function LoginScreen() {
   return (
     <View>
       <Text>LoginScreen</Text>
+      <TextInput
+      placeholder='Ingresar correo:'
+      />
     </View>
   )
 }
